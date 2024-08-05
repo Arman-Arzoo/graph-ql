8dbe1119be147204a1893b9b64c40f64a71ffdaf
Sheraz Hassan <toplinegeeks@gmail.com>
2024-08-05 18:56:54 +0500

    connect with fake db and play around with data

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 // types
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
 // server setup
 const server = new ApolloServer({
   // typeDefs => definition of types of data
   typeDefs,
-  // resolvers
+  resolvers,
 });
 const port = 4000;
 
